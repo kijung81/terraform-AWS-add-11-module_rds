89bb864b382efd5330ad9af8cbcfa274cf700eb9
MZC01-MYNECK <mzc01-myneck@MZC01-MYNECK.local>
2023-10-19 18:06:25 +0900

    11module-rds

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable prefix {}
+
+variable sg_id {}
+
+variable dbpri_subnet_a_id {}
+
+variable dbpri_subnet_c_id {}
+
+variable db_ins_cls {}
+
+variable rds_dbname {}
+
+variable rds_username {}
+
+variable rds_password {}
